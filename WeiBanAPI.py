607d47a043dcbf3f541d637839fab15948f4014e
Trent <44160771+wqyzstql@users.noreply.github.com>
2020-10-20 11:34:12 +0800

    Add files via upload

---
+++ WeiBanAPI.py
@@ -0,0 +1,139 @@
+from urllib import parse
+import json
+import random
+import time
+import requests
+import os
+
+baseDelayTime = 1  # 基础延时秒数
+
+randomDelayDeviation = 1  # 叠加随机延时差
+
+getCookiesURL = 'https://weiban.mycourse.cn/#/login'  # 请求Cookies URL
+
+loginURL = 'https://weiban.mycourse.cn/pharos/login/login.do'  # 登录请求 URL
+
+getNameURL = 'https://weiban.mycourse.cn/pharos/my/getInfo.do'  # 请求姓名 URL
+
+getProgressURL = 'https://weiban.mycourse.cn/pharos/project/showProgress.do'  # 请求进度 URL
+
+getListCourseURL = 'https://weiban.mycourse.cn/pharos/usercourse/listCategory.do'  # 请求课程种类 URL
+
+getListURL = 'https://weiban.mycourse.cn/pharos/usercourse/listCourse.do' # 请求课程列表URL
+
+finishCourseURL = 'https://weiban.mycourse.cn/pharos/usercourse/finish.do'  # 请求完成课程URL
+
+getRandImageURL = 'https://weiban.mycourse.cn/pharos/login/randImage.do'  # 验证码URL
+
+doStudyURL = 'https://weiban.mycourse.cn/pharos/usercourse/study.do'  # 学习课程URL
+
+genQRCodeURL = 'https://weiban.mycourse.cn/pharos/login/genBarCodeImageAndCacheUuid.do'  # 获取验证码以及验证码ID URL
+
+loginStatusURL = 'https://weiban.mycourse.cn/pharos/login/barCodeWebAutoLogin.do'  # 用于二维码登录刷新登录状态
+
+
+# 二维码登录
+def qrLogin():
+    qrCodeID = getQRCode()
+    
+    try:
+        while True:
+            responseText = getLoginStatus(qrCodeID)
+            responseJSON = json.loads(responseText)
+            if responseJSON['code'] == '0':
+                return responseJSON
+            else:
+                print('未登录，等待后5s刷新')
+                time.sleep(5)
+    except KeyboardInterrupt:
+        print('用户中止程序运行')
+
+
+# 获取学生信息
+def getStuInfo(userId, tenantCode):
+    print('开始请求用户数据')
+    param = {
+        'userId': userId,
+        'tenantCode': tenantCode
+    }
+    req = requests.post(url=getNameURL, data=param)
+    responseJSON = json.loads(req.text)
+    return responseJSON
+
+
+# 获取课程进度
+def getProgress(userProjectId, tenantCode):
+    param = {
+        'userProjectId': userProjectId,
+        'tenantCode': tenantCode
+    }
+    req = requests.post(url=getProgressURL, data=param)
+    responseJSON = json.loads(req.text)
+    return responseJSON
+
+
+# 获取课程列表
+def getListCourse(userProjectId, chooseType, tenantCode):
+    param = {
+        'userProjectId': userProjectId,
+        'chooseType': chooseType,
+        'tenantCode': tenantCode,
+    }
+    req = requests.post(url=getListCourseURL, data=param)
+    responseJSON = json.loads(req.text)
+    return responseJSON
+
+def GetList(userProjectId,  categoryCode ,chooseType, tenantCode, name):
+    param = {
+        'userProjectId': userProjectId,
+        'categoryCode': categoryCode,
+        'chooseType': chooseType,
+        'tenantCode': tenantCode,
+        'name': name
+    }
+    req = requests.post(url=getListURL, data=param)
+    responseJSON = json.loads(req.text)
+    return responseJSON
+
+# 完成课程请求
+def finishCourse(userCourseId, tenantCode):
+    param = {
+        'userCourseId': userCourseId,
+        'tenantCode': tenantCode,
+    }
+    url_values = parse.urlencode(param)  # GET请求URL参数
+    req = requests.get(url=finishCourseURL + '?' + url_values)
+    print(req.text)
+
+
+def getRandomTime():
+    return baseDelayTime + random.randint(0, randomDelayDeviation)
+
+
+def doStudy(userProjectId, userCourseId, tenantCode):
+    param = {
+        'userProjectId': userProjectId,
+        'courseId': userCourseId,
+        'tenantCode': tenantCode
+    }
+    req = requests.post(url=doStudyURL, data=param)
+    print(req.text)
+    return
+
+
+# 获取并返回QRCode 链接以及 QRCode ID
+def getQRCode():
+    req = requests.post(url=genQRCodeURL)
+    responseJSON = json.loads(req.text)
+    print('请扫描二维码登录')
+    os.system('explorer {}'.format(responseJSON['data']['imagePath']))
+    return responseJSON['data']['barCodeCacheUserId']
+
+
+# 用于二维码登录，刷新是否已经成功登录
+def getLoginStatus(qrCodeID):
+    param = {
+        'barCodeCacheUserId': qrCodeID
+    }
+    req = requests.post(url=loginStatusURL, data=param)
+    return req.text
---
+++ main.py
@@ -0,0 +1,73 @@
+import WeiBanAPI
+import json
+import time  # time.sleep延时
+import os  # 兼容文件系统
+import random
+
+tenantCode = '4137011066'  # 院校ID
+
+
+def main():
+    loginResponse = WeiBanAPI.qrLogin()
+
+    try:
+        print('登录成功，userName:' + loginResponse['data']['userName'])
+    except BaseException:
+        print('登录失败')
+        print(loginResponse)  # TODO: 这里的loginResponse调用没有考虑网络错误等问题
+        exit(0)
+
+    # 请求解析并打印用户信息
+    try:
+        print('请求用户信息')
+        stuInfoResponse = WeiBanAPI.getStuInfo(loginResponse['data']['userId'],
+                                               tenantCode)
+        print('用户信息：' + stuInfoResponse['data']['realName'] + '\n'
+              + stuInfoResponse['data']['orgName']
+              + stuInfoResponse['data']['specialtyName']
+              )
+    except BaseException:
+        print('解析用户信息失败，将尝试继续运行，请注意运行异常')
+
+    # 请求课程完成进度
+    try:
+        getProgressResponse = WeiBanAPI.getProgress(loginResponse['data']['preUserProjectId'],
+                                                    tenantCode)
+        print('课程总数：' + str(getProgressResponse['data']['requiredNum']) + '\n'
+              + '完成课程：' + str(getProgressResponse['data']['requiredFinishedNum']) + '\n'
+              + '结束时间' + str(getProgressResponse['data']['endTime']) + '\n'
+              + '剩余天数' + str(getProgressResponse['data']['lastDays'])
+              )
+    except BaseException:
+        print('解析课程进度失败，将尝试继续运行，请注意运行异常')
+
+    # 请求课程列表
+    try:
+        getListCourseResponse = WeiBanAPI.getListCourse(loginResponse['data']['preUserProjectId'],
+                                                        '3',
+                                                        tenantCode)
+    except BaseException:
+        print('请求课程列表失败')
+
+    print('解析课程列表并发送完成请求')
+
+    for i in getListCourseResponse['data']:
+        print('\n----章节码：' + i['categoryCode'] + '章节内容：' + i['categoryName'])
+        NowClass = WeiBanAPI.GetList(loginResponse['data']['preUserProjectId'],
+                                                        i['categoryCode'],
+                                                        '3',
+                                                        tenantCode,
+                                                        '')
+        for j in NowClass['data']:
+            print('课程内容：' + j['resourceName'] + '\nuserCourseId:' + j['userCourseId'])
+
+            if (j['finished'] == 1):
+                print('已完成')
+            else:
+                print('发送完成请求')
+                WeiBanAPI.doStudy(loginResponse['data']['preUserProjectId'], j['resourceId'], tenantCode)
+                WeiBanAPI.finishCourse(j['userCourseId'], tenantCode)
+
+
+if __name__ == '__main__':
+    main()
